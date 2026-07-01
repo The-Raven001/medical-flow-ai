5081de324301b1350079b249b100ca40e49efb1a
Gabriel <gacm95218@gmail.com>
2026-07-01 16:03:30 -0400

    Created mockup of register website

---
+++ frontend/app/pages/Register.tsx
@@ -0,0 +1,35 @@
+
+
+
+export function Register() {
+
+    return (
+        <div className="flex flex-col items-center mt-20">
+            <h1 className="text-2xl mb-6">User register</h1>
+
+            <form 
+            className="flex flex-col gap-4 w-80">
+
+                <input type="text"
+                placeholder="username"
+                className="border p-2 rounded"
+                required />
+
+                <input 
+                type="email"
+                placeholder="email"
+                className="border p-2 rounded"
+                required />
+
+                <input type="password"
+                placeholder="password"
+                className="border p-2 rounded"
+                required />
+                
+                <button type="submit"
+                ></button>
+            </form>
+            <p>Already have an account?</p>
+        </div>
+    )
+}
